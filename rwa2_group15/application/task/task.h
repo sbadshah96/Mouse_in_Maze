0a327096827c010297044c2ae7210323aba2e287
sbadshah <sbadshah@umd.edu>
2022-11-05 23:30:03 -0400

    8th update from sbadshah to the team
    Added more documentation

--- rwa2_group15/application/task/task.h
+++ rwa2_group15/application/task/task.h
@@ -1,10 +1,26 @@
 #ifndef __TASK_H__
 #define __TASK_H__
 
+/**
+ * @file task.h
+ * @author Shreejay Badshah (sbadshah@umd.edu)
+ * @brief Functions to move the mouse in a maze, keeping track of mouse's location and direction, and setting maze cell data in an array.
+ * @version 1.0
+ * @date 2022-11-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include<string>
 #include<iostream>
 #include<array>
 #include<memory>
+
+/**
+ * @brief Working under rwa2group15 namespace.
+ * 
+ */
 
 namespace rwa2group15{
 
@@ -21,24 +37,49 @@ class Mouse{
                                                              m_direction{direction}{
                //  std::cerr << "Starting position: (0,0), facing north." << std::endl;
         }
-     // turn robot to left side and move
+
+     /**
+      * @brief Turn robot to left side and move
+      * 
+      * 
+      */
      void turn_left();
      
-     // turn robot to right side and move
+     /**
+      * @brief Turn robot to right side and move
+      * 
+      * 
+      */
      void turn_right();
 
-     // move robot forward
+     /**
+      * @brief Move robot forward
+      * 
+      * 
+      */
      void move_forward();
 
-     // turn robot to back side and move
+     /**
+      * @brief Turn robot to back side and move
+      * 
+      * 
+      */
      void turn_back();
 
-     // return the direction of the robot
+     /**
+      * @brief Get the direction object
+      * 
+      * @return const char& 
+      */
      const char& get_direction(){                     
         return m_direction;
      }
 
-     // return the position of the robot
+     /**
+      * @brief Get the position object
+      * 
+      * @return const std::pair<int, int>& 
+      */
      const std::pair<int, int>& get_position(){       
         return m_position;
      }
@@ -50,7 +91,7 @@ class Mouse{
 }; // class Mouse
 
 /**
- * @brief This class represents a cell (walls) in the maze.
+ * @brief This class represents a cell (walls) in the maze
  * 
  */
 class Cell{
@@ -61,13 +102,29 @@ class Cell{
             // std::cout << "Initializing cell walls." << std::endl;
         }
      
-     // initialize all 4 wall of a cell
+     /**
+      * @brief Initialize all 4 wall of a cell
+      * 
+      * 
+      */
      void init_cell_walls();
 
-     // set one of the walls based on is_wall
+     /**
+      * @brief Set the wall object based on is_wall method
+      * 
+      * @param wall 
+      * @param is_wall 
+      */
      void set_wall(int wall, bool is_wall);
      
-     // return true if wall is closed
+     /**
+      * @brief Check if the wall exists or not
+      * 
+      * @param wall 
+      * @param direction 
+      * @return true 
+      * @return false 
+      */
      bool is_wall(int wall, char direction);
 
      void get_walls(){
@@ -103,37 +160,67 @@ class Algorithm{
          // std::cerr << "Starting Algorithm Class" << std::endl;
       }
 
-     // initialize outer walls, generate goals, execute search algorithm etc
+     /**
+      * @brief Initialize outer walls, generate goals, and execute search algorithm
+      * 
+      * 
+      */
      void run();
 
-     // color outer walls
+     /**
+      * @brief Color outer walls
+      * 
+      * 
+      */
      void init_outer_walls();
 
-     // algorithm for left hand rule approach
+     /**
+      * @brief Algorithm for left hand rule approach
+      * 
+      * 
+      */
      void follow_wall_left();
 
-     // algorithm for right hand rule approach
+     /**
+      * @brief Algorithm for right hand rule approach
+      * 
+      * 
+      */
      void follow_wall_right();
 
-     // generate random goal adjacent to an outer wall
+     /**
+      * @brief Generate random goal adjacent to an outer wall
+      * 
+      * 
+      */
      void generate_goal();
 
-     // set left wall in m_maze and simulator
+     /**
+      * @brief Set left wall in m_maze and simulator
+      * 
+      * 
+      */
      void set_left_wall();
 
-     // set right wall in m_maze and simulator
+     /**
+      * @brief Set the right wall object in m_maze and simulator
+      * 
+      * 
+      */
      void set_right_wall();
-
-     // set front wall in m_maze and simulator
+ 
+     /**
+      * @brief Set the front wall object in m_maze and simulator
+      * 
+      * 
+      */
      void set_front_wall();
 
-     // set the color of the cell on Mouse's path.
+     /**
+      * @brief Set the color of the cell on Mouse's path.
+      * 
+      */
      void set_cell_color();
-
-     //
-   //   void set_mouse(std::shared_ptr<Mouse> mouse){
-   //       m_mouse = mouse;
-   //   }
 
      private:
      //!<@brief A maze is a 2D array of cells.
